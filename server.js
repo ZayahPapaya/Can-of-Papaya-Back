2b8b478c9acdc1fda6b65af376f5f8c94efc8994
Aiden <hyena.shenanigans@gmail.com>
2022-05-30 22:05:24 -0700

    Build in api

--- server.js
+++ server.js
@@ -3,16 +3,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Book = require('./book');
 
 const app = express();
 app.use(cors());
 
+mongoose.connect(process.env.DBURL)
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected')
+});
 const PORT = process.env.PORT || 3001;
 
 app.get('/test', (request, response) => {
-
   response.send('test request received')
-
-})
-
+});
+app.get('/library', async (request, response) => {
+  const books = await Book.find({});
+  // const books = await Book.find({author: 'Wizards RPG Team});
+  response.send(books);
+});
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
